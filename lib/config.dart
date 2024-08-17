5ca03b1996c645f52c46f29286980af0f0418e01
Aditya <adityaraj61522@gmail.com>
2024-08-18 03:40:20 +0530

    fix

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
-  static const String apiBaseUrl = 'http://localhost:7000';
-  // static const String apiBaseUrl = 'https://api.linket.chat';
+  // static const String apiBaseUrl = 'http://localhost:7000';
+  static const String apiBaseUrl = 'https://api.linket.chat';
   // static const String socketBaseUrl = 'http://192.168.0.202:7000';
   static const String socketBaseUrl = 'https://api.linket.chat';
 }
