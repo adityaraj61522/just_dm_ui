2aa49057c0722a18ff9971feeb85e6d107dce6d7
Aditya <adityaraj61522@gmail.com>
2024-08-20 03:30:16 +0530

    fixed  web socket  connection

--- lib/config.dart
+++ lib/config.dart
@@ -2,5 +2,5 @@ class Config {
   // static const String apiBaseUrl = 'http://localhost:7000';
   static const String apiBaseUrl = 'https://api.linket.chat';
   // static const String socketBaseUrl = 'http://192.168.0.202:7000';
-  static const String socketBaseUrl = 'https://api.linket.chat';
+  static const String socketBaseUrl = 'http://45.148.28.152:7000';
 }
