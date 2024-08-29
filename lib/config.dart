59bb8ab067dfb565a7a46dbd30eebcb55853c70f
Aditya <adityaraj61522@gmail.com>
2024-08-30 05:15:12 +0530

    config updated

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,9 @@
 class Config {
-  // static const String apiBaseUrl = 'http://localhost:7005';
+  // static const String apiBaseUrl = 'http://localhost:7000';
   static const String apiBaseUrl = 'https://api.linket.chat';
   // static const String socketBaseUrl = 'http://192.168.0.202:7000';
   static const String socketBaseUrl = 'https://api.linket.chat';
+  static const String bucketAccessKey = '46043078d0609431c37fd7e0502c055d';
+  static const String bucketSecretKey =
+      '3ad21c7c90222f0e282dc65aea38829f3597165cc8732f57924bc8287b6c8a41';
 }
